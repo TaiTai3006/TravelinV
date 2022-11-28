c89319afe90d5fc8f4f28a2417412cf0c8824873
leethiep <lethithiep131002@gmail.com>
2022-11-28 22:50:02 +0700

    Revert "update formblog"
    This reverts commit 9fce7f1a2515027d4d630ac34c174fc022aa40a1.

--- frontend/src/Pages/FormBlog.js
+++ frontend/src/Pages/FormBlog.js
@@ -7,27 +7,20 @@ import { BiPlus } from "react-icons/bi";
 import Header from "../components/Header";
 import Input from "../components/Input";
 import dataProvince from "../dataProvince";
-import { AiOutlineClose } from "react-icons/ai";
-
 
 function FormBlog() {
   const [selectedImage, setSelectedImage] = useState(null);
 
   const [inputList, setInputList] = useState([<Input />]);
   const onAddBtnClick = (event) => {
+    console.log('add', inputList)
     setInputList(inputList.concat(<Input key={inputList.length} />));
-    
   };
-  const onRemoveBtnClick = (event,index) => {
-
-    setInputList(inputList.filter((input) => 
-      input.key == index || input.key == 'null' && index == 0
-    ));
-    
-  }
-
-  const [provinces, setProvinces] = useState(dataProvince);
-
+  const [provinces, setProvinces] = useState(dataProvince)
+  
+  
+  
+  
   return (
     <>
       <Header />
@@ -41,91 +34,85 @@ function FormBlog() {
       </div>
       <div className="heading ">
         <h1>Create post </h1>
+       
       </div>
       {/* {inputList} */}
       <form>
-        <div className="form">
-          <div className="container-image ">
-            <label for="input-img" className="preview">
-              <BsFileImage className="preview--icon" />
+      <div className="form">
+      
+        <div className="container-image ">
+          <label for="input-img" className="preview">
+            <BsFileImage className="preview--icon" />
 
-              <span>Upload your image for post here </span>
-              {selectedImage && (
-                <div>
-                  <img
-                    alt="not fount"
-                    width={"100%"}
-                    src={URL.createObjectURL(selectedImage)}
-                  />
-                </div>
-              )}
-            </label>
-            <input
-              type="file"
-              hidden
-              id="input-img"
-              onChange={(event) => {
-                setSelectedImage(event.target.files[0]);
-              }}
-            />
-            <div className="submit">
-              <input type="submit" className="submit" value="POST"></input>
-            </div>
-            {/* <button className="submit">Submit</button> */}
+            <span>Upload your image for post here </span>
+            {selectedImage && (
+              <div>
+                <img
+                  alt="not fount"
+                  width={"100%"}
+                  src={URL.createObjectURL(selectedImage)}
+                />
+              </div>
+            )}
+          </label>
+          <input
+            type="file"
+            hidden
+            id="input-img"
+            onChange={(event) => {
+              setSelectedImage(event.target.files[0]);
+            }}
+          />
+          <div className="submit">
+            <input type="submit" className="submit" value="POST"></input>
           </div>
-          <div class="input">
-            <div className="palace-list">
-              {" "}
-              <label for="palace">Choose palace:</label>
-              <select name="palace" id="palace" required>
-                <option value="">None</option>
-                {provinces.map((province) => {
-                  // console.log(province)
-                  return (
-                    <>
-                      <option value={province.slug}>{province.name}</option>
-                    </>
-                  );
-                })}
-              </select>
-            </div>
+          {/* <button className="submit">Submit</button> */}
+        </div>
+        <div class="input">
+         
+          <div className="palace-list">
+          {" "}
+          <label for="palace">Choose palace:</label>
+         
+          <select name="palace" id="palace" required>
+          <option value="">None</option>
+            {provinces.map((province)=>{
+              console.log(province)
+             return (
+              <>
+              
+              <option value={province.slug}>{province.name}</option>
+              </>
+             )
+            })}
 
-            <h3>Title </h3>
-            <input
-              type="text"
-              class="title"
-              placeholder="Write the title of your blog ....."
-              required
-            ></input>
-            <textarea
-              id="message"
-              name="message"
-              placeholder="Write the title of your blog ....."
-              required
-            />
-            <div className="description">
-              {inputList.map((input,id) => {
-                return <>
-                <div className="remove" onClick={(e) => onRemoveBtnClick(e,id)}>
-                  Click here to remove this description
-                </div>
-                <Input/>
+          </select>
+        </div>
+        <h3>Title </h3>
+          <input
+            type="text"
+            class="title"
+            placeholder="Write the title of your blog ....."
+            required
+          ></input>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Write the title of your blog ....."
+            required
+          />
 
-                </>;
-              })}
-
-              {/* <Input inputList = {inputList}/> */}
-            </div>
-            <div className="addDesc">
-              <h3>Add more description</h3>
-            </div>
-            <div>
-              <button onClick={onAddBtnClick} className="add-btn">
-                <BiPlus className="addDesc--icon" />
-              </button>
-            </div>
+          <div className="description">{inputList}</div>
+          <div className="addDesc">
+            <h3>Add more description</h3>
+          </div>
+          <div>
+            <button onClick={onAddBtnClick} className="add-btn">
+              <BiPlus className="addDesc--icon" />
+            </button>
           </div>
         </div>
+      </div>
       </form>
     </>
   );
