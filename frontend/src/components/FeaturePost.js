363e9d5aae203a9aecc71127240dc5b468d88a0a
sontran-test <sontran347123@gmail.com>
2022-12-07 13:42:51 +0700

    home_backend

--- frontend/src/components/FeaturePost.js
+++ frontend/src/components/FeaturePost.js
@@ -1,33 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import dalat from "../image/dalat.jpg";
+import Axios, * as others from 'axios';
 
 function FeaturePost() {
-  const [posts, setpost] = useState([
-    {
-      id: 1,
-      title: "10 Best Places To Visit In Da Lat ",
-      img: "https://i.pinimg.com/564x/49/61/c4/4961c45f5411a2dc19a9e57918742e48.jpg",
-    },
-    {
-      id: 2,
-      title: "10 Best Places To Visit In Da Lat ",
-      img: "https://i.pinimg.com/564x/41/4d/a3/414da3bc207e7956fa256f8db19b7574.jpg",
-    },
-    {
-      id: 3,
-      title: "10 Best Places To Visit In Da Lat ",
-      img: "https://i.pinimg.com/564x/de/bc/bf/debcbfce7461d56be7a3e0897028ccfd.jpg",
-    },
-  ]);
+  const [posts, setpost] = useState([])
+
+ useEffect(() =>{
+   Axios.get("http://localhost:8800/home/featuredpost").then((response)=>{
+      setpost(response.data)
+     }
+   )
+ },[])
   return (
     <div className="feature_post--container">
       {posts.map((posts) => {
-        const { id, title, img } = posts;
+        const { idPost, postName, image } = posts;
         return (
-          <a href="#" className="feature_post" key={id}>
-            <h2 className="feature_post--title">{title} </h2>
+          <a href="#" className="feature_post" >
+            <h2 className="feature_post--title">{postName} </h2>
             <div className="feature_post--img">
-              <img src={img} alt={title} />
+              <img src={image} alt={postName} />
             </div>
           </a>
         );
--- frontend/src/components/RelatedPost.js
+++ frontend/src/components/RelatedPost.js
@@ -1,117 +1,125 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import related1 from "../image/danang.jpg";
 
 import related2 from "../image/rl2.jpg";
 import dalat from "../image/dalat.jpg";
-
+import Axios, * as others from 'axios';
 function RelatedPost() {
   const [posts1, setPost1] = useState([
-    {
-      id: 1,
-      img: { related2 },
-      title: " 27 Mistakes You Wish Not To Make When Traveling Solo",
-    },
-    {
-      id: 2,
-      img: "../../src/image/dalat.jpg",
-      title: " 27 Mistakes You Wish Not To Make When Traveling Solo",
-    },
-    {
-      id: 3,
-      img: "../../src/image/dalat.jpg",
-      title: " 27 Mistakes You Wish Not To Make When Traveling Solo",
-    },
-    {
-      id: 4,
-      img: "../../src/image/dalat.jpg",
-      title: " 27 Mistakes You Wish Not To Make When Traveling Solo",
-    },
-    {
-      id: 5,
-      img: "../../src/image/dalat.jpg",
-      title: " 27 Mistakes You Wish Not To Make When Traveling Solo",
-    },
-    {
-      id: 6,
-      img: "../../src/image/dalat.jpg",
-      title: " 27 Mistakes You Wish Not To Make When Traveling Solo",
-    },
-    {
-      id: 7,
-      img: "../../src/image/dalat.jpg",
-      title: " 27 Mistakes You Wish Not To Make When Traveling Solo",
-    },
-    {
-      id: 8,
-      img: "../../src/image/dalat.jpg",
-      title: " 27 Mistakes You Wish Not To Make When Traveling Solo",
-    },
-    {
-      id: 9,
-      img: "../../src/image/dalat.jpg",
-      title: " 27 Mistakes You Wish Not To Make When Traveling Solo",
-    },
+    // {
+    //   id: 1,
+    //   img: { related2 },
+    //   title: " 27 Mistakes You Wish Not To Make When Traveling Solo",
+    // },
+    // {
+    //   id: 2,
+    //   img: "../../src/image/dalat.jpg",
+    //   title: " 27 Mistakes You Wish Not To Make When Traveling Solo",
+    // },
+    // {
+    //   id: 3,
+    //   img: "../../src/image/dalat.jpg",
+    //   title: " 27 Mistakes You Wish Not To Make When Traveling Solo",
+    // },
+    // {
+    //   id: 4,
+    //   img: "../../src/image/dalat.jpg",
+    //   title: " 27 Mistakes You Wish Not To Make When Traveling Solo",
+    // },
+    // {
+    //   id: 5,
+    //   img: "../../src/image/dalat.jpg",
+    //   title: " 27 Mistakes You Wish Not To Make When Traveling Solo",
+    // },
+    // {
+    //   id: 6,
+    //   img: "../../src/image/dalat.jpg",
+    //   title: " 27 Mistakes You Wish Not To Make When Traveling Solo",
+    // },
+    // {
+    //   id: 7,
+    //   img: "../../src/image/dalat.jpg",
+    //   title: " 27 Mistakes You Wish Not To Make When Traveling Solo",
+    // },
+    // {
+    //   id: 8,
+    //   img: "../../src/image/dalat.jpg",
+    //   title: " 27 Mistakes You Wish Not To Make When Traveling Solo",
+    // },
+    // {
+    //   id: 9,
+    //   img: "../../src/image/dalat.jpg",
+    //   title: " 27 Mistakes You Wish Not To Make When Traveling Solo",
+    // },
   ]);
+
   const [posts2, setPost2] = useState([
-    {
-      id: 1,
-      img: { dalat },
-      provinc: " Da Nang",
-    },
-    {
-      id: 2,
-      img: { dalat },
-      provinc: " Da Nang",
-    },
-    {
-      id: 3,
-      img: { related1 },
-      provinc: " Da Nang",
-    },
-    {
-      id: 4,
-      img: { dalat },
-      provinc: " Da Nang",
-    },
-    {
-      id: 5,
-      img: { related1 },
-      provinc: " Da Nang",
-    },
-    {
-      id: 6,
-      img: { related1 },
-      provinc: " Da Nang",
-    },
-    {
-      id: 7,
-      img: { related1 },
-      provinc: " Da Nang",
-    },
-    {
-      id: 8,
-      img: { related1 },
-      provinc: " Da Nang",
-    },
-    {
-      id: 9,
-      img: { related1 },
-      provinc: " Da Nang",
-    },
+    // {
+    //   id: 1,
+    //   img: { dalat },
+    //   provinc: " Da Nang",
+    // },
+    // {
+    //   id: 2,
+    //   img: { dalat },
+    //   provinc: " Da Nang",
+    // },
+    // {
+    //   id: 3,
+    //   img: { related1 },
+    //   provinc: " Da Nang",
+    // },
+    // {
+    //   id: 4,
+    //   img: { dalat },
+    //   provinc: " Da Nang",
+    // },
+    // {
+    //   id: 5,
+    //   img: { related1 },
+    //   provinc: " Da Nang",
+    // },
+    // {
+    //   id: 6,
+    //   img: { related1 },
+    //   provinc: " Da Nang",
+    // },
+    // {
+    //   id: 7,
+    //   img: { related1 },
+    //   provinc: " Da Nang",
+    // },
+    // {
+    //   id: 8,
+    //   img: { related1 },
+    //   provinc: " Da Nang",
+    // },
+    // {
+    //   id: 9,
+    //   img: { related1 },
+    //   provinc: " Da Nang",
+    // },
   ]);
+  useEffect(()=>{
+    Axios.get('http://localhost:8800/home/relatedpost').then((response) => {
+      setPost1(response.data)
+      // setPost2(response.data)
+    })
+  })
   return (
     <div className="related-post--container">
       <div className="related-post--content">
         {posts1.map((post) => {
-          const { id, title, img } = post;
+          const { id, postName, image } = post;
           return (
             <a href="#" className="related-post--item" key={id}>
               {/* <img src={img} alt={post.title} /> */}
               <div className="realted-post--item-image">
-                <img src={window.location.origin + related2} />
+                {/* <img src={window.location.origin + related2} /> */}
+                <img src={image} ></img>
               </div>
               <div className="realted-post--item-title">
-                <p> {title}</p>
+                <p> {postName}</p>
               </div>
             </a>
           );
@@ -119,8 +127,8 @@ function RelatedPost() {
       </div>
 
       <div className="related-post--suggestion">
-        {posts2.map((post) => {
-          const { id, provinc, img } = post;
+        {posts1.map((post) => {
+          const { id, provinceName, image } = post;
           return (
             <>
               <a
@@ -129,9 +137,9 @@ function RelatedPost() {
                 key={id}
                 // style={{ backgroundImage:`url(${process.env.PUBLIC_URL+ img})` }}
               >
-                <img src={window.location.origin + related1} />
+                <img src={image} />
                 {/* <img src={dalat}  width="250" height="400"/> */}
-                <div className="province">{provinc}</div>
+                <div className="province">{provinceName}</div>
               </a>
               {/* <a href="#" className="suggestion--item back" key={id}>
                 <h1>John Doe</h1>
