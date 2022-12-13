edb5db2c4a40f828931b97aae1df69f3e9d0736d
SonUIT347 <94755840+SonUIT347@users.noreply.github.com>
2022-12-13 16:12:39 +0700

    update_deletepost

--- Back-end/controllers/adminController.js
+++ Back-end/controllers/adminController.js
@@ -2,10 +2,10 @@ import { db } from "../index.js";
 
 
 export const getpost_user = ((req,res)=>{
-    const sqlget = `SELECT userName, postName, (dateTime), status, image FROM post 
-                                                                            ORDER BY (CASE status WHEN 'pending' THEN 1
-                                                                            WHEN 'approved'THEN 2 
-                                                                            ELSE 100 END) ASC, dateTime DESC`
+    const sqlget = `SELECT * FROM post
+                            ORDER BY (CASE status WHEN 'pending' THEN 1
+                            WHEN 'approved'THEN 2 
+                            ELSE 100 END) ASC, dateTime DESC`
     db.query(sqlget, (err, result)=>{
         res.json(result)
     })
